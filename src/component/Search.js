3e3d8bc5ce4bf99f9e04f6ef8af77106499b89c6
shipu11b <mshipu143@gmail.com>
2020-06-05 21:19:44 -0400

    third commit

--- src/component/Search.js
+++ src/component/Search.js
@@ -13,14 +13,15 @@ class Search extends Component {
     handlesearchchange = () => {
         const search = this.state.input;
         const API_KEY = process.env.REACT_APP_SEARCH_API_KEY;
-        const url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}/`;
+        const url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}`;
         axios.get(url,{params: {key:API_KEY}})
                 .then((response) => {
                 const data = response.data;
                 console.log(response.data);
                 this.setState({
-                    search: data, input: data.title,
+                    search: [[data.data[0].images.downsized_large.url ]],input : this.state.input
                 });
+                
             })
             .catch((err) => {console.log(err)
             this.setState({search: [ ]})
@@ -32,25 +33,25 @@ class Search extends Component {
     }
 
     render(){
+        let  display = (
+            <ul >
+                {this.state.search.map((x,y) => (
+                <img key={y} src ={x} alt = "GIF"></img>
+              ))}
+          </ul>
+          );
+    
         return (
             <center>
             <div>
-                A world of gif'search
-                <br/>
-                <input type = "text" value={this.state.input} onChange={this.handleInput}/>
-                {" "}
-                <button onClick= {this.handlesearchchange}>search</button> 
-                {
-                    this.state.search.map((item,index) => (
-                        <div key={index}>
-                        <h3>here's your gif {item.title} {item.type} {item.url} </h3>
-                        </div>
-                        )                        
-                    )
-                }
-            </div>
+            <h3>A world of Gif</h3>
+             <input type = "text" value={this.state.input} onChange={this.handleInput}/>
+             <button onClick= {this.handlesearchchange}>search</button> 
+            {display}
+​
+            </div> 
             </center>
-        )
+        );
     }
 }
 
